a43e222d90a68d39eec6516eee8ef6f530d11611
Moshe Ben-Zacharia <BravoMoshe@MacbookPro15s-MacBook-Pro.local>
2016-08-20 15:38:43 -0400

    Added OVR unity utilities, utility scripts, main scene, project settings, cameras

---
+++ Assets/Scripts/Utility/AnimatedSpriteUtility.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Utility
+{
+	public class AnimatedSpriteUtility : MonoBehaviour
+	{
+		//readonly
+
+		//Serialized
+		public float updateInterval = .013f;
+		public bool sharedMaterial;
+		public bool pingPong;
+		public string propertyName = "_MainTex";
+		public bool autoIncrement = true;
+		
+		/////Protected/////
+		//References
+		protected Material material;
+		//Primitives
+		protected Vector2 offsetInterval;
+		protected int totalIntervals;
+		protected float lastUpdateTime;
+		protected int resetIndex;
+		protected int direction = 1;
+		public int myIndex = 0;
+		
+		
+
+		///////////////////////////////////////////////////////////////////////////
+		//
+		// Inherited from MonoBehaviour
+		//
+		
+		protected void Awake()
+		{
+			if(sharedMaterial)
+				material = GetComponent<MeshRenderer>().sharedMaterial;
+			else
+				material = GetComponent<MeshRenderer>().material;
+
+			offsetInterval = material.GetTextureScale(propertyName);
+			totalIntervals = (int) (1f/offsetInterval.x);
+			resetIndex = totalIntervals*totalIntervals;
+		}
+		
+		protected void Start ()
+		{
+
+		}
+		
+		protected void Update ()
+		{
+			
+			if (autoIncrement && Time.time - lastUpdateTime > updateInterval) {
+
+				float timeElapsed = Time.time - this.lastUpdateTime;
+				
+				this.myIndex += direction*((int) (timeElapsed/updateInterval));
+				
+				if (this.myIndex >= this.resetIndex && direction > 0) {
+					if(this.pingPong)
+					{
+						this.direction = -1;
+					}
+					else
+					{
+						this.myIndex = 0;
+					}
+				}
+				if (this.myIndex <= 0 && direction < 0) {
+					if(this.pingPong)
+					{
+						this.direction = 1;
+					}
+				}
+
+				OnIndexUpdated();
+
+				this.lastUpdateTime = Time.time;
+			}
+//			Debug.Log (this.myIndex);
+		}
+		
+		///////////////////////////////////////////////////////////////////////////
+		//
+		// Utility_AnimatedSprite Functions
+		//
+
+		protected void OnIndexUpdated() {
+
+			int x = 0;
+			int y = 0;
+
+			if(this.myIndex!=0)
+			{
+				x = this.myIndex%(totalIntervals);
+				y = this.myIndex/(totalIntervals);
+			}
+
+			SetTextureOffset(Vector2.Scale(offsetInterval,new Vector2(x,totalIntervals- (y+1))));
+		}
+
+		protected void SetTextureOffset(Vector2 offset) {
+			this.material.SetTextureOffset(propertyName, offset);
+		}
+
+		public void SetPercent(float percent) {
+
+//			Debug.Log(percent);
+			this.myIndex = (int) (percent*((float) (this.resetIndex-1)));
+			OnIndexUpdated();
+		}
+	}
+}
---
+++ Assets/Scripts/Utility/Editor/ChildSwitcherUtilityEditor.cs
@@ -0,0 +1,71 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace App
+{
+	[ExecuteInEditMode]
+	[CustomEditor (typeof(ChildSwitcherUtility))]
+	class ChildSwitcherUtilityEditor : Editor {
+		
+		Transform[] children ;
+		void OnEnable(){
+			Transform myTransform = ((ChildSwitcherUtility) target).transform;
+			children = new Transform[myTransform.childCount];
+			for (int i=0; i<children.Length; ++i) {
+				children [i] = myTransform.GetChild (i);
+			}
+		}
+		static readonly float disableColorValue=.85f;
+		Color disabledColor = new Color(disableColorValue,disableColorValue,disableColorValue);
+		public override void OnInspectorGUI ()
+		{
+			GUILayout.Space(10);
+			GUILayout.BeginHorizontal ();
+
+			ChildSwitcherUtility tracker = (ChildSwitcherUtility) target;
+
+			for(int i=0;i<children.Length;++i)
+			{
+				GUIStyle currentStyle = EditorStyles.miniButtonMid;
+				if(i==0)
+					currentStyle=EditorStyles.miniButtonLeft;
+				if(i==children.Length-1)
+					currentStyle=EditorStyles.miniButtonRight;
+				if(!children[i].gameObject.activeSelf){
+					GUI.backgroundColor=disabledColor;
+						GUI.color=disabledColor;
+				}
+				if(GUILayout.Button(children[i].gameObject.name,currentStyle)){
+					tracker.SetActive(i);
+				}
+				GUI.backgroundColor=Color.white;
+				GUI.color=Color.white;
+			}
+
+			GUILayout.EndHorizontal ();
+			GUILayout.Space(10);
+		}
+
+
+		[MenuItem("GameObject/Select Child Switcher Child #t", false, 0)]
+		static void SelectTrackerCamera ()
+		{
+			Transform tracker = GameObject.Find("Cameras").transform;
+			for(int i=0;i<tracker.childCount;++i)
+			{
+				if(tracker.GetChild(i).gameObject.activeSelf)
+					Selection.activeTransform = tracker.GetChild(i);
+			}
+		}
+
+		[MenuItem("GameObject/Child Switcher/Select Child Switcher #%t", false, 0)]
+		static void SelectTracker ()
+		{
+			Transform tracker = GameObject.Find("Cameras").transform;
+
+			Selection.activeTransform = tracker;
+		}
+	}
+}
